52058c21e6866590968d86db69e99b2bb932790f
Jakub Baran <jakub.baran2@gmail.com>
2022-07-27 13:32:38 +0200

    final-app

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+
 
 let count = 0;
 
 function increment() {
     count += 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 function save() {
-    console.log(count);
+    let entires = count + " - "
+    saveEl.textContent += entires
+
+}
+
+function backToZero () {
+    countEl.textContent = 0
 }
